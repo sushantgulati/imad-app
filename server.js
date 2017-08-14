fc1aaa57d5ca4f3ee92f9a6430d350d70182bf2a
sushantgulati <30962198+sushantgulati@users.noreply.github.com>
2017-08-14 06:38:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'Article One | Sushant Gulati',
+    date: '14th August 2017',
+    heading: 'Article One',
+    content: `<p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
+        
+        
+            <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
+        
+        
+            <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
+    `
+};
+
+var htmlTemplate=
+  `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>    
+    <body>
+        <div class= "container">
+        <div>
+            <a href= '/'>
+              Home  
+            </a>
+        </div>
+        
+        <div>
+            <hr/>
+            <h3> ${heading} </h3>
+        </div>
+        
+        <div>
+            ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+        </div>
+    
+    </body>
+    
+        
+</html>
+;
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
