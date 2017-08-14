baef95298ec3f73432c1289dcf73edd0be8aef73
sushantgulati <30962198+sushantgulati@users.noreply.github.com>
2017-08-14 06:47:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ var articleOne = {
             <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
     `
 };
+function createTemplate (data) { 
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
 
 var htmlTemplate=
   `<html>
@@ -50,9 +55,11 @@ var htmlTemplate=
     
     </body>
     
-        
-</html>
-;
+    </html>
+    ;
+return htmlTemplate;
+
+};
 
 
 app.get('/', function (req, res) {
@@ -60,7 +67,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req, res) {
